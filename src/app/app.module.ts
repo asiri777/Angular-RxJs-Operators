4eb309933f65051a0bb12f0fd7bffbf10b351e2f
Asiri Jayaweera <asandaruwan64@gmail.com>
2020-01-11 09:15:01 +0530

    adding home component to core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 
 
 
 @NgModule({
-  declarations: [NavComponent],
+  declarations: [HomeComponent, NavComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  exports: [HomeComponent, NavComponent]
 })
 export class CoreModule { }
