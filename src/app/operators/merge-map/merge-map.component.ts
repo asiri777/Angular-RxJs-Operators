154d755a6762ac81c4d03fb11b4ccef8ba9d4195
Asiri Jayaweera <asandaruwan64@gmail.com>
2020-01-11 15:53:45 +0530

    completed MergeMapComponent

--- src/app/operators/merge-map/merge-map.component.ts
+++ src/app/operators/merge-map/merge-map.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { of, from } from 'rxjs';
+import { delay, map, mergeAll, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-merge-map',
   templateUrl: './merge-map.component.html',
   styleUrls: ['./merge-map.component.css']
 })
-export class MergeMapComponent implements OnInit {
+export class MergeMapComponent {
+  constructor() {
+    const getData = (param) => {
+      return of(`retrieved new data with param ${param}`).pipe(delay(1000));
+    }
+    // // using a regular map
+    // from([1, 2, 3, 4]).pipe(
+    //   map(param => getData(param))
+    // ).subscribe(val => val.subscribe(data => console.log(data)));
 
-  constructor() { }
+    // // using map and mergeAll
+    // from([1, 2, 3, 4]).pipe(
+    //   map(param => getData(param)),
+    //   mergeAll()
+    // ).subscribe(val => console.log(val));
 
-  ngOnInit() {
+    // using mergeMap
+    from([1, 2, 3, 4]).pipe(
+      mergeMap(param => getData(param))
+    ).subscribe(val => console.log(val));
   }
-
 }
