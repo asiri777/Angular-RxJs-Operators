c93398147af39d70642aa004e60edc39f6474e0f
Asiri Jayaweera <asandaruwan64@gmail.com>
2020-01-11 17:40:53 +0530

    Completed ConcatMapComponent

--- src/app/operators/concat-map/concat-map.component.ts
+++ src/app/operators/concat-map/concat-map.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { of, from } from 'rxjs';
+import { delay, map, mergeMap, concatMap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-concat-map',
   templateUrl: './concat-map.component.html',
   styleUrls: ['./concat-map.component.css']
 })
-export class ConcatMapComponent implements OnInit {
+export class ConcatMapComponent {
 
-  constructor() { }
+  constructor() {
+    const getData = (param) => {
+      const delayTime = Math.floor(Math.random() * 10000) + 1;
+      return of(`retrieved new data with params: ${param} and delay: ${delayTime}`).pipe(
+        delay(delayTime)
+      );
+    };
 
-  ngOnInit() {
+    // // using a regular map
+    // from([1, 2, 3, 4]).pipe(
+    //   map(param => getData(param))
+    // ).subscribe(val => val.subscribe(data => console.log('map:', data)));
+
+    // // using mergeMap
+    // from([1, 2, 3, 4]).pipe(
+    //   mergeMap(param => getData(param))
+    // ).subscribe(val => console.log('mergeMap:', val));
+
+    // // using switchMap
+    // from([1, 2, 3, 4]).pipe(
+    //   switchMap(param => getData(param))
+    // ).subscribe(val => console.log('concatMap:', val));
+
+    // using concatMap
+    from([1, 2, 3, 4]).pipe(
+      concatMap(param => getData(param))
+    ).subscribe(val => console.log('concatMap:', val));
+
   }
-
 }
--- src/app/operators/operators-routing.module.ts
+++ src/app/operators/operators-routing.module.ts
@@ -5,11 +5,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { MapComponent } from './map/map.component';
 import { MergeMapComponent } from './merge-map/merge-map.component';
 import { SwitchMapComponent } from './switch-map/switch-map.component';
+import { ConcatMapComponent } from './concat-map/concat-map.component';
 
 const routes: Routes = [
   { path: 'map', component: MapComponent },
   { path: 'mergeMap', component: MergeMapComponent },
-  { path: 'switchMap', component: SwitchMapComponent }
+  { path: 'switchMap', component: SwitchMapComponent },
+  { path: 'concatMap', component: ConcatMapComponent }
 ];
 
 @NgModule({
