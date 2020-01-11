db81c65d1c6330b964b77d55a12c330c8b841c77
Asiri Jayaweera <asandaruwan64@gmail.com>
2020-01-11 10:43:01 +0530

    editing map component

--- src/app/operators/map/map.component.ts
+++ src/app/operators/map/map.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { State, states } from 'src/app/shared/state';
+import { Vehicle, vehicles } from 'src/app/shared/vehicle';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent {
+  states$: Observable<State[]>;
+  vehicle$: Observable<Vehicle[]>;
 
-  constructor() { }
+  constructor() {
+    this.states$ = of(states);
+    this.states$.pipe(
+      map(stateList => stateList.map(state => `${state.abbreviation} - ${state.name}`)))
+      .subscribe(stateList => console.log(stateList));
 
-  ngOnInit() {
+    this.vehicle$ = of(vehicles);
+    this.vehicle$.pipe(
+      map(vehicleList => vehicleList.map(vehicle => `${vehicle.brand} ${vehicle.model}`))
+    ).subscribe(vehicle => console.log(vehicle));
+    this.vehicle$.pipe(
+      map(vehicleList => vehicleList.filter(vehicle => vehicle.brand === 'porsche'))
+    ).subscribe(porsche => console.log(porsche));
   }
 
 }
---
+++ src/app/shared/vehicle.ts
@@ -0,0 +1,23 @@
+export interface Vehicle {
+  brand: string;
+  model: string;
+}
+
+export const vehicles = [
+  {
+    brand: 'porsche',
+    model: '911'
+  },
+  {
+    brand: 'porsche',
+    model: 'macan'
+  },
+  {
+    brand: 'ferarri',
+    model: '458'
+  },
+  {
+    brand: 'lamborghini',
+    model: 'urus'
+  }
+];
