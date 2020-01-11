627640cb0513f04a8179b3a760273f34546dca52
Asiri Jayaweera <asandaruwan64@gmail.com>
2020-01-11 18:53:26 +0530

    added service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    HttpClientModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/operators/map/map.component.ts
+++ src/app/operators/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { State, states } from 'src/app/shared/state';
@@ -13,8 +14,10 @@ export class MapComponent {
   states$: Observable<State[]>;
   vehicle$: Observable<Vehicle[]>;
 
-  constructor() {
-    this.states$ = of(states);
+  constructor(private http: HttpClient) {
+    // this.states$ = of(states);
+    this.states$ = http.get<State[]>('http://localhost:3000/states');
+
     this.states$.pipe(
       map(stateList => stateList.map(state => `${state.abbreviation} - ${state.name}`)))
       .subscribe(stateList => console.log(stateList));
