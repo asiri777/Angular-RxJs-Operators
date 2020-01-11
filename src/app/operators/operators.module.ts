8483d6108027d3b3776f353ba1671471a94c7268
Asiri Jayaweera <asandaruwan64@gmail.com>
2020-01-11 10:50:46 +0530

    adding Merge Map Component

--- src/app/operators/operators.module.ts
+++ src/app/operators/operators.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './map/map.component';
 import { OperatorsRoutingModule } from './operators-routing.module';
+import { MergeMapComponent } from './merge-map/merge-map.component';
 
 
 
 @NgModule({
-  declarations: [MapComponent],
+  declarations: [MapComponent, MergeMapComponent],
   imports: [
     CommonModule,
     OperatorsRoutingModule
